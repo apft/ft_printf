f95696fa77c8758ae430fe0173817f305af177ef
Alexandre PION <apion@student.42.fr>
2019-02-05 13:57:25 +0100

    improve test script generator + Makefile

--- test/test_printf/gen.sh
+++ test/test_printf/gen.sh
@@ -2,6 +2,8 @@
 
 debug=1;
 
+data="int_format.txt"
+
 template_folder="template"
 template_launcher=${template_folder}/00_launcher.c
 template_header=${template_folder}/test_int.h
@@ -22,73 +24,135 @@ then
 fi
 
 includes="<stdio.h> <string.h> <stdlib.h> \"ft_printf.h\" \"utils.h\""
-
 variables="\tint\t\terror;\n\tchar\t*format;\n\tchar\tstr_printf[BUFF_SIZE];\n\tchar\t*out;\n"
 
+read_block ()
+{
+	in_block=-1;
+	while read data_line
+	do
+		[ "${data_line}" = "end_block" ] && in_block=0;
+		if [ ${in_block} -eq 1 ]
+		then
+			loop=$((loop + 1))
+			[ $loop -le 9 ] && prefix="00$loop"
+			[ $loop -ge 10 -a $loop -le 99 ] && prefix="0$loop"
+			[ $loop -ge 100 ] && prefix=$loop
+			title=`echo ${data_line} | cut -d ',' -f 1 | sed "s/#/$1 ${count[$(get_index_count $type)]}/"`
+			proto=`echo ${data_line} | cut -d ',' -f 2`
+			proto="${proto}_${type}_${count[$(get_index_count $type)]}"
+			type_var=`echo ${data_line} | cut -d ',' -f 3`
+			value=`echo ${data_line} | cut -d ',' -f 4`
+			file="${prefix}_${proto}_${count[$(get_index_count $type)]}.c"
+			nbr_arg=$(( `echo $format | tr -dc '|' | wc -c` - 1))
+			#[ $debug -eq 1 ] && echo $type   ${count[$(get_index_count $type)]}   $title   $proto   $value   $file   $nbr_arg
+			if [ ! -f "$folder/$file" ]
+			then
+				[ $debug -eq 1 ] && echo "generating file: $file"
+				{
+					for inc in $includes
+					do
+						echo "#include $inc"
+					done
+					echo
+					echo "int\t\t$proto(void)"
+					echo "{\n$variables\t${type_var}\t\tn;\n"
+					echo "\tbzero(str_printf, BUFF_SIZE);"
+					echo "\tformat = $format;"
+					echo "\tn = $value;"
+					printf "\tsprintf(str_printf, format"
+					for i in `seq ${nbr_arg}`
+					do
+						printf ", n"
+					done
+					echo ");"
+					printf "\tft_printf_str(&out, format"
+					for i in `seq ${nbr_arg}`
+					do
+						printf ", n"
+					done
+					echo ");\n"
+					echo "\terror = strcmp(str_printf, out);"
+					echo "\tif (error)"
+					echo "\t\tprint_diff(format, str_printf, out);"
+					echo "\tfree(out);"
+					echo "\treturn (error);"
+					echo "}"
+				} > $folder/$file
+				[ $debug -eq 1 ] && echo "add to launcher"
+				n_line=`cat $launcher | wc -l | tr -d ' '`
+				n=$((n_line - 2))
+				{
+					head -n $n $launcher
+					echo "\tadd_test(&test_list, $title, &$proto);"
+					tail -n 2 $launcher
+				} > $launcher.tmp
+				mv $launcher.tmp $launcher
+				[ $debug -eq 1 ] && echo "add to header"
+				n_line=`cat $header | wc -l | tr -d ' '`
+				n=$((n_line - 2))
+				{
+					head -n $n $header
+					echo "int\t\t$proto(void);"
+					tail -n 2 $header
+				} > $header.tmp
+				mv $header.tmp $header
+			else
+				echo "nothing to be done for file: $file"
+			fi
+		fi
+		[ "${data_line}" = "start_block" ] && in_block=1;
+	done < ${data_block}
+}
+
+get_index_count()
+{
+	id=0;
+	[ $1 = "d" ] && id=0
+	[ $1 = "i" ] && id=1
+	[ $1 = "ld" ] && id=2
+	[ $1 = "lld" ] && id=3
+	[ $1 = "u" ] && id=4
+	[ $1 = "lu" ] && id=5
+	[ $1 = "llu" ] && id=6
+	[ $1 = "x" ] && id=7
+	[ $1 = "X" ] && id=8
+	[ $1 = "o" ] && id=9
+	echo $id
+}
+
+inc_count()
+{
+	[ $1 = "d" ] && let count[0]+=1
+	[ $1 = "i" ] && let count[1]+=1
+	[ $1 = "ld" ] && let count[2]+=1
+	[ $1 = "lld" ] && let count[3]+=1
+	[ $1 = "u" ] && let count[4]+=1
+	[ $1 = "lu" ] && let count[5]+=1
+	[ $1 = "llu" ] && let count[6]+=1
+	[ $1 = "x" ] && let count[7]+=1
+	[ $1 = "X" ] && let count[8]+=1
+	[ $1 = "o" ] && let count[9]+=1
+}
+
+data_block="data_block.txt"
+end="end_block"
+[ $debug -eq 1 ] && echo "extract data block"
+head -n $(grep -n $end $data | cut -d ':' -f 1 | head -n 1) $data > ${data_block}
+
 loop=0;
+block=-1;
+# count: d i ld ld u lu llu x X o
+count=(0 0 0 0 0 0 0 0 0 0)
 while read line
 do
-	loop=$((loop + 1))
-	[ $loop -le 9 ] && prefix="0$loop" || prefix=$loop
-	title=`echo $line | cut -d ',' -f 1`
-	name=`echo $line | cut -d ',' -f 2`
-	file="${prefix}_${name}.c"
-	type=`echo $line | cut -d ',' -f 3`
-	nbr_arg=`echo $line | cut -d ',' -f 4`
-	format=`echo $line | cut -d ',' -f 5`
-	value=`echo $line | cut -d ',' -f 6`
-	if [ ! -f "$folder/$file" ]
+	[ "$line" = "start_block" ] && block=1;
+	if [ $block -eq 0 ]
 	then
-		[ $debug -eq 1 ] && echo "generating file: $file"
-		{
-			for inc in $includes
-			do
-				echo "#include $inc"
-			done
-			echo
-			echo "int\t\t$name(void)"
-			echo "{\n$variables\t$type\t\tn;\n"
-			echo "\tbzero(str_printf, BUFF_SIZE);"
-			echo "\tformat = $format;"
-			echo "\tn = $value;"
-			printf "\tsprintf(str_printf, format"
-			for i in `seq ${nbr_arg}`
-			do
-				printf ", n"
-			done
-			echo ");"
-			printf "\tft_printf_str(&out, format"
-			for i in `seq ${nbr_arg}`
-			do
-				printf ", n"
-			done
-			echo ");\n"
-			echo "\terror = strcmp(str_printf, out);"
-			echo "\tif (error)"
-			echo "\t\tprint_diff(format, str_printf, out);"
-			echo "\tfree(out);"
-			echo "\treturn (error);"
-			echo "}"
-		} > $folder/$file
-		[ $debug -eq 1 ] && echo "add to launcher"
-		n_line=`wc -l $launcher | cut -d ' ' -f 7`
-		n=$((n_line - 2))
-		{
-			head -n $n $launcher
-			echo "\tadd_test(&test_list, $title, &$name);"
-			tail -n 2 $launcher
-		} > $launcher.tmp
-		mv $launcher.tmp $launcher
-		[ $debug -eq 1 ] && echo "add to header"
-		n_line=`wc -l $header | cut -d ' ' -f 7`
-		n=$((n_line - 2))
-		{
-			head -n $n $header
-			echo "int\t\t$name(void);"
-			tail -n 2 $header
-		} > $header.tmp
-		mv $header.tmp $header
-	else
-		echo "nothing to be done for file: $file"
+		type=`echo $line | cut -d ',' -f 1`
+		format=`echo $line | cut -d ',' -f 2`
+		read_block $type
+		let count[$(get_index_count $type)]+=1
 	fi
+	[ "$line" = "end_block" ] && block=0;
 done < int_format.txt
