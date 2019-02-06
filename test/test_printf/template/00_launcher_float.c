1fc2bac5d223f2484428e01a1095207d668d189b
Alexandre PION <apion@student.42.fr>
2019-02-06 18:18:12 +0100

    forgotten templates for float tests

---
+++ test/test_printf/template/00_launcher_float.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   00_launcher_float.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/05 20:06:08 by apion             #+#    #+#             */
+/*   Updated: 2019/02/06 18:04:35 by apion            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libunit.h"
+#include "test_str_char.h"
+
+int		test_extract_float_conv_hex_launcher(void)
+{
+	t_unit_test	*test_list;
+
+	test_list = 0;
+	return (launch_tests("TEST FLOAT %a %A", &test_list));
+}
