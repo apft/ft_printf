75851fd0ef5624e685810cd98dfbe649acd3d45a
Alexandre PION <apion@student.42.fr>
2019-02-13 14:40:51 +0100

    fix norme

--- srcs/handlers/handle_int_conv.c
+++ srcs/handlers/handle_int_conv.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 22:48:35 by apion             #+#    #+#             */
-/*   Updated: 2019/02/13 00:29:22 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/13 14:38:54 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int		handle_int_conv_u(union u_value *value, t_specs *specs, char *str)
 	if (specs->flags & MOD_H)
 		return (handle_int_conv_ushort(value->type_ushort, specs, str));
 	if (specs->flags & MOD_L)
-		return (handle_int_conv_ulong(value->type_ulong,specs, str));
+		return (handle_int_conv_ulong(value->type_ulong, specs, str));
 	if (specs->flags & MOD_LL)
 		return (handle_int_conv_ulong_long(value->type_ulong_long, specs, str));
 	return (handle_int_conv_uint(value->type_uint, specs, str));
