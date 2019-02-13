7a2c836f00634410170359b3c1b69fdbdc1b2951
Alexandre PION <apion@student.42.fr>
2019-02-13 15:42:35 +0100

    fix norme

--- incs/handlers.h
+++ incs/handlers.h
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 00:05:04 by apion             #+#    #+#             */
-/*   Updated: 2019/02/13 15:23:17 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/13 15:42:14 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		handle_int_conv(union u_value *value, t_specs *specs, char *str);
 int		handle_int_conv_char(char value, t_specs *specs, char *str);
 int		handle_int_conv_short(short value, t_specs *specs, char *str);
 int		handle_int_conv_int(int value, t_specs *specs, char *str);
-int		handle_int_conv_long(long  value, t_specs *specs, char *str);
+int		handle_int_conv_long(long value, t_specs *specs, char *str);
 int		handle_int_conv_long_long(long long value, t_specs *s, char *str);
 
 int		handle_int_conv_u(union u_value *value, t_specs *specs, char *str);
