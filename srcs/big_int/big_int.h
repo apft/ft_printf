0e6304a9bdca2e9585862f27dba990767d1e6b49
Alexandre PION <apion@student.42.fr>
2019-02-14 13:55:10 +0100

    move header to module dir + refactor names

--- srcs/big_int/big_int.h
+++ srcs/big_int/big_int.h
@@ -6,19 +6,20 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 12:48:57 by apion             #+#    #+#             */
-/*   Updated: 2019/02/14 13:08:30 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/14 13:57:56 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BIG_INT_H
 # define BIG_INT_H
 
-# define BIG_INT_BLOCKS_SIZE 4
+# define BIGINT_N_BLOCKS 4
+# define BIGINT_SIZE_BLOCK 32
 
 typedef struct	s_bigint
 {
 	int		length;
-	int		blocks[BIG_INT_BLOCKS_SIZE + 1];
+	int		blocks[BIGINT_N_BLOCKS + 1];
 }				t_bigint;
 
 #endif
