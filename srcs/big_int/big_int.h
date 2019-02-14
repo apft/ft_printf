f919964b91260cf9e3f2d28fcfa6172557e29324
Alexandre PION <apion@student.42.fr>
2019-02-14 18:28:43 +0100

    prototypes + improve types in header struct

--- srcs/big_int/big_int.h
+++ srcs/big_int/big_int.h
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 12:48:57 by apion             #+#    #+#             */
-/*   Updated: 2019/02/14 13:57:56 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/14 17:29:10 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,18 @@
 # define BIGINT_N_BLOCKS 4
 # define BIGINT_SIZE_BLOCK 32
 
+# define BIGINT_OVERFLOW 0xf
+# define BIGINT_UNDERFLOW 0xff
+
 typedef struct	s_bigint
 {
-	int		length;
-	int		blocks[BIGINT_N_BLOCKS + 1];
+	unsigned int	length;
+	unsigned int	blocks[BIGINT_N_BLOCKS + 1];
 }				t_bigint;
+
+void			print_big_int(t_bigint *value);
+void			bigint_add(t_bigint *r, t_bigint *a, t_bigint *b);
+void			bigint_shift_left(t_bigint *r, t_bigint *a, unsigned int shift);
+void			bigint_shift_left_self(t_bigint *a, unsigned int shift);
 
 #endif
