5e8b771be02056ef6f676d94ebde69bc648c5c2a
Alexandre PION <apion@student.42.fr>
2019-02-15 15:03:27 +0100

    [test] explode makefile sources

---
+++ test/test_printf/makefiles/makefile_ptr_percent.mk
@@ -0,0 +1,17 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile_ptr_percent                               :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: apion <apion@student.42.fr>                +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/02/15 14:40:06 by apion             #+#    #+#              #
+#    Updated: 2019/02/15 14:40:08 by apion            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+C_FILES_PTR_PERCENT := test_ptr_percent/00_launcher.c \
+						test_ptr_percent/01_test_ptr.c \
+						test_ptr_percent/02_test_ptr_null.c \
+						test_ptr_percent/03_test_percent.c \
+						test_ptr_percent/04_test_percent_hard.c
---
+++ test/test_printf/makefiles/makefile_str_char.mk
@@ -0,0 +1,26 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile_str_char                                  :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: apion <apion@student.42.fr>                +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/02/15 14:39:55 by apion             #+#    #+#              #
+#    Updated: 2019/02/15 14:39:58 by apion            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+C_FILES_STR := test_str_char/00_launcher.c \
+				test_str_char/01_test_char_easy.c \
+				test_str_char/02_test_char_medium.c \
+				test_str_char/03_test_char_hard.c \
+				test_str_char/04_test_char_null.c \
+				test_str_char/05_test_string_medium.c \
+				test_str_char/06_test_string_medium_other.c \
+				test_str_char/07_test_string_hard.c \
+				test_str_char/08_test_string_null_mix.c \
+				test_str_char/09_test_str_char_null.c \
+				test_str_char/10_test_mix_all_null.c \
+				test_str_char/11_test_mix_null_medium.c \
+				test_str_char/12_test_undef_type.c \
+				test_str_char/13_test_undef_type_other.c
