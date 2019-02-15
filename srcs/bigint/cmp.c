9ffb0224dadc0c7880214b3904a99dab284664f9
Alexandre PION <apion@student.42.fr>
2019-02-15 16:20:40 +0100

    fix bigint_cmp logic

--- srcs/bigint/cmp.c
+++ srcs/bigint/cmp.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 12:24:36 by apion             #+#    #+#             */
-/*   Updated: 2019/02/15 12:38:20 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/15 16:17:50 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,12 @@ int		bigint_cmp(t_bigint *a, t_bigint *b)
 		return (-1);
 	if (a->length > b->length)
 		return (1);
-	block = 0;
-	while (block < a->length)
+	block = a->length;
+	while (block--)
 	{
 		diff = a->blocks[block] - b->blocks[block];
 		if (diff)
 			return (diff);
-		++block;
 	}
 	return (0);
 }
