f40d02c6c2eb47baccc2e4985a21d84ef7e76990
Alexandre PION <apion@student.42.fr>
2019-02-15 12:06:45 +0100

    test add print_diff_bigint + refactor utils

--- test/test_printf/utils.c
+++ test/test_printf/utils.c
@@ -6,21 +6,39 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 15:23:11 by apion             #+#    #+#             */
-/*   Updated: 2019/02/07 15:04:49 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/15 12:06:00 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "bigint.h"
 
-void	print_diff(char *format, int ret, int exp, char *expected, char *result, int diff)
+void	print_diff(char *format, int length_return, int length_expexted,
+					char *expected, char *result, int diff)
 {
-	int		err;
+	int		length_diff;
+	char	*color;
+	char	*no_color;
 
-	err = ret != exp;
+	length_diff = length_return != length_expected;
+	color = length_diff ? "\e[33m" : "";
+	no_color = length_diff ? "\e[0m" : "";
 	printf("\e[1;4mformat:\e[0m %s\n", format);
-	printf("  --size ret : %s%d%s\n", err ? "\e[33m" : "", ret, err ? "\e[0m" : "");
-	printf("  --size exp : %d\n", exp);
+	printf("  --size ret : %s%d%s\n", color, length_return, no_color);
+	printf("  --size exp : %d\n", length_expected);
 	printf("  --str ret  : %s\n", result);
 	printf("  --str exp  : %s\n", expected);
 	printf("  --diff cmp : %d\n", diff);
+}
+
+void	print_diff_bigint(t_bigint *a, t_bigint *b, t_bigint *result,
+	   					t_bigint *expected, int diff)
+{
+	printf("cmp(result, expected) = %d\n", diff);
+	printf("a\tb\n");
+	print_bigint(a);
+	print_bigint(b);
+	printf("result\texpected\n");
+	print_bigint(result);
+	print_bigint(expected);
 }
