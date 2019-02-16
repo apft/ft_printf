5c642b5a28a68289219651d3c8fcdf4849283e85
Alexandre PION <apion@student.42.fr>
2019-02-16 12:56:54 +0100

    improve bigint add

--- srcs/bigint/add.c
+++ srcs/bigint/add.c
@@ -6,38 +6,55 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 16:03:40 by apion             #+#    #+#             */
-/*   Updated: 2019/02/15 11:31:21 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/08 13:10:50 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bigint.h"
 
-void	bigint_add(t_bigint *result, t_bigint *a, t_bigint *b)
+static int	sum_and_propagate_carry(t_bigint *result, t_bigint *a, t_bigint *b)
 {
-	int				length;
-	int				block;
+	unsigned int	i;
 	unsigned long	carry;
 	unsigned long	sum;
 
-	length = a->length < b ->length ? b->length : a->length;
-	result->length = length;
-	block = 0;
-	carry = 0L;
-	while (block < length)
+	i = 0;
+	carry = 0UL;
+	while (i < result->length)
 	{
-		sum = carry + (unsigned long)(a->blocks[block]) + (unsigned long)(b->blocks[block]);
+		sum = carry;
+		sum += (unsigned long)(a->blocks[i]) + (unsigned long)(b->blocks[i]);
 		carry = sum >> BIGINT_SIZE_BLOCK;
-		result->blocks[block] = sum & 0xffffffff;
-		++block;
+		result->blocks[i] = sum & BIGINT_MASK_BLOCK;
+		++i;
 	}
-	if (carry)
+	return (carry ? 1 : 0);
+}
+
+void		bigint_add(t_bigint *result, t_bigint *a, t_bigint *b)
+{
+	if (bigint_is_overflow(a) || bigint_is_overflow(b))
+		result->blocks[BIGINT_N_BLOCKS] = BIGINT_OVERFLOW;
+	else
 	{
-		if (result->length < BIGINT_N_BLOCKS)
+		result->length = (a->length < b->length) ? b->length : a->length;
+		if (sum_and_propagate_carry(result, a, b))
 		{
-			result->blocks[block] = 1;
-			result->length += 1;
+			if (result->length < BIGINT_N_BLOCKS)
+			{
+				result->blocks[result->length] = 1;
+				result->length += 1;
+			}
+			else
+				result->blocks[BIGINT_N_BLOCKS] = BIGINT_OVERFLOW;
 		}
-		else
-			result->blocks[block] = BIGINT_OVERFLOW;
 	}
+}
+
+void		bigint_add_int(t_bigint *result, t_bigint *a, unsigned int n)
+{
+	t_bigint	b;
+
+	bigint_init_int(&b, n);
+	bigint_add(result, a, &b);
 }
