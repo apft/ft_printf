c10dee275b4ad84c5d6fc27a5d4e508bb5835e54
Alexandre PION <apion@student.42.fr>
2019-02-16 16:29:44 +0100

    refactor for norme

--- srcs/bigint/shift.c
+++ srcs/bigint/shift.c
@@ -6,38 +6,45 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 16:58:42 by apion             #+#    #+#             */
-/*   Updated: 2019/02/16 14:34:56 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/16 16:30:03 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bigint.h"
 
-void	bigint_shift_left_self(t_bigint *result, unsigned int shift)
+static int	is_shift_overflow(unsigned int block_id, unsigned int modulo)
+{
+	if (!modulo && block_id > BIGINT_N_BLOCKS)
+		return (1);
+	if (modulo && block_id >= BIGINT_N_BLOCKS)
+		return (1);
+	return (0);
+}
+
+void		bigint_shift_left_self(t_bigint *result, unsigned int shift)
 {
 	bigint_shift_left(result, result, shift);
 }
 
-void	bigint_shift_left(t_bigint *result, t_bigint *input, unsigned int shift)
+void		bigint_shift_left(t_bigint *result, t_bigint *input,
+								unsigned int shift)
 {
 	unsigned int	offset;
 	unsigned int	mod;
-	unsigned int	i;
 	unsigned int	carry;
+	unsigned int	i;
 
 	offset = shift / BIGINT_SIZE_BLOCK;
 	mod = shift % BIGINT_SIZE_BLOCK;
 	i = input->length;
-	if ((!mod && i + offset > BIGINT_N_BLOCKS)
-				|| (mod && i + offset >= BIGINT_N_BLOCKS))
+	if (is_shift_overflow(i + offset, mod))
 		result->blocks[BIGINT_N_BLOCKS] = BIGINT_OVERFLOW;
 	else
 	{
 		result->length = i + offset;
 		while (i--)
 		{
-			carry = 0;
-			if (mod)
-				carry = input->blocks[i] >> (BIGINT_SIZE_BLOCK - mod);
+			carry = mod ? input->blocks[i] >> (BIGINT_SIZE_BLOCK - mod) : 0;
 			result->blocks[i + offset + 1] += carry;
 			result->blocks[i + offset] = input->blocks[i] << mod;
 			if ((i + 1) == input->length && carry)
