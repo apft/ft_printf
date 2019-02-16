a2d25ca06952ffdf6eb9ea54d8db3501fb978872
Alexandre PION <apion@student.42.fr>
2019-02-16 12:06:45 +0100

    [test] improve tests for bigint cmp

--- test/test_printf/test_bigint/test_cmp/05_test_bigint_cmp_length_sup.c
+++ test/test_printf/test_bigint/test_cmp/05_test_bigint_cmp_length_sup.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 15:53:01 by apion             #+#    #+#             */
-/*   Updated: 2019/02/15 16:07:26 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/16 12:04:46 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		test_bigint_cmp_length_sup(void)
 	b = (t_bigint){2, {42, 42, 0, 0, 0}};
 	
 	diff = bigint_cmp(&a, &b);
-	err = !(diff > 0);
+	err = !(diff == 1);
 	if (err)
 		print_diff_bigint(&a, &b, 0, 0, diff);	
 	return (err);
