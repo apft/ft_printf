b2c7a93e0669304b3d4f2e6249f0d2ad246d9516
Alexandre PION <apion@student.42.fr>
2019-02-16 12:06:27 +0100

    [test] bigint init

--- test/test_printf/test_bigint/test_bigint.h
+++ test/test_printf/test_bigint/test_bigint.h
@@ -6,13 +6,14 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 11:02:24 by apion             #+#    #+#             */
-/*   Updated: 2019/02/15 15:08:28 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/16 11:53:12 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEST_BIGINT_H
 # define TEST_BIGINT_H
 
+int		test_bigint_init_launcher(void);
 int		test_bigint_cmp_launcher(void);
 int		test_bigint_add_launcher(void);
 
---
+++ test/test_printf/test_bigint/test_init/02_test_bigint_init_int.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   02_test_bigint_init_int.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/16 11:42:26 by apion             #+#    #+#             */
+/*   Updated: 2019/02/16 12:03:28 by apion            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+#include "bigint.h"
+
+int		test_bigint_init_int(void)
+{
+	t_bigint		a;
+	unsigned int	n;
+	int				err;
+	int				i;
+
+	n = 42;
+	bigint_init_int(&a, n);
+	err = 0;
+	i = 1;
+	while (!err && i < BIGINT_N_BLOCKS)
+		err += a.blocks[i++];
+	err += !(a.blocks[0]  == n) && !(a.length == 1);
+	if (err)
+	{
+		printf("a should be %#x = %u\n", n, n);
+		printf("length = %d\n", a.length);
+		print_bigint(&a);
+	}
+	return (err);
+}
