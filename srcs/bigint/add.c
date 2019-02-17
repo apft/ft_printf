6bfad40497587b699f86dc494db945f795903e37
Alexandre PION <apion@student.42.fr>
2019-02-17 11:43:15 +0100

    bigint add refactoring

--- srcs/bigint/add.c
+++ srcs/bigint/add.c
@@ -6,19 +6,18 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 16:03:40 by apion             #+#    #+#             */
-/*   Updated: 2019/02/16 13:09:25 by apion            ###   ########.fr       */
+/*   Updated: 2019/02/17 11:50:21 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bigint.h"
 
-void	bigint_add(t_bigint *result, t_bigint *a, t_bigint *b)
+static int	sum_and_propagate_carry(t_bigint *result, t_bigint *a, t_bigint *b)
 {
 	int				i;
 	unsigned long	carry;
 	unsigned long	sum;
 
-	result->length = (a->length < b->length) ? b->length : a->length;
 	i = 0;
 	carry = 0UL;
 	while (i < result->length)
@@ -29,19 +28,29 @@ void	bigint_add(t_bigint *result, t_bigint *a, t_bigint *b)
 		result->blocks[i] = sum & BIGINT_MASK_BLOCK;
 		++i;
 	}
-	if (carry)
-	{
-		if (result->length < BIGINT_N_BLOCKS)
+	return (carry ? 1 : 0);
+}
+
+void		bigint_add(t_bigint *result, t_bigint *a, t_bigint *b)
+{
+	if (bigint_is_overflow(a) || bigint_is_overflow(b))
+		result->blocks[BIGINT_N_BLOCKS] = BIGINT_OVERFLOW;
+	else {
+		result->length = (a->length < b->length) ? b->length : a->length;
+		if (sum_and_propagate_carry(result, a , b))
 		{
-			result->blocks[i] = carry;
-			result->length += 1;
+			if (result->length < BIGINT_N_BLOCKS)
+			{
+				result->blocks[result->length] = 1;
+				result->length += 1;
+			}
+			else
+				result->blocks[BIGINT_N_BLOCKS] = BIGINT_OVERFLOW;
 		}
-		else
-			result->blocks[i] = BIGINT_OVERFLOW;
 	}
 }
 
-void	bigint_add_int(t_bigint *result, t_bigint *a, unsigned int n)
+void		bigint_add_int(t_bigint *result, t_bigint *a, unsigned int n)
 {
 	t_bigint	b;
 
