85f3f47eea0ec6e1509f1df59f172e91eca2c4f8
Alexandre PION <apion@student.42.fr>
2019-03-01 19:41:33 +0100

    test: improve bigint sub test - use roundtrip with add

--- test/test_printf/test_bigint/test_sub/01_test_bigint_sub_bigint.c
+++ test/test_printf/test_bigint/test_sub/01_test_bigint_sub_bigint.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 11:11:12 by apion             #+#    #+#             */
-/*   Updated: 2019/02/16 18:11:14 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/01 19:26:02 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,18 @@ int		test_bigint_sub_bigint(void)
 	t_bigint	a;
 	t_bigint	b;
 	t_bigint	result;
-	t_bigint	expected;
+	t_bigint	round_trip;
 	int			diff;	
 
 	a = (t_bigint){2, {420, 789, 0, 0, 0}};
 	b = (t_bigint){1, {256, 0, 0, 0, 0}};
 	bigint_init_null(&result);
-	expected = (t_bigint){2, {420 - 256, 789, 0, 0, 0}};
+	bigint_init_null(&round_trip);
 
 	bigint_sub(&result, &a, &b);
-	diff = bigint_cmp(&result, &expected);
+	bigint_add(&round_trip, &b, &result);
+	diff = bigint_cmp(&round_trip, &a);
 	if (diff)
-		print_diff_bigint(&a, &b, &result, &expected, diff);	
+		print_diff_bigint(&a, &b, &result, &round_trip, diff);	
 	return (diff);
 }
