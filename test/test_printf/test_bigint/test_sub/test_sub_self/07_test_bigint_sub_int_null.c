857328e7f03b78908a8ac26f196c6a4c1117fb79
Alexandre PION <apion@student.42.fr>
2019-03-07 18:48:40 +0100

    test: bigint sub self storage + subfolders

--- test/test_printf/test_bigint/test_sub/test_sub_self/07_test_bigint_sub_int_null.c
+++ test/test_printf/test_bigint/test_sub/test_sub_self/07_test_bigint_sub_int_null.c
@@ -1,19 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_bigint_sub.h                                  :+:      :+:    :+:   */
+/*   07_test_bigint_sub_int_null.c                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/15 11:02:24 by apion             #+#    #+#             */
-/*   Updated: 2019/03/07 18:42:42 by apion            ###   ########.fr       */
+/*   Created: 2019/02/15 17:14:32 by apion             #+#    #+#             */
+/*   Updated: 2019/03/07 18:36:14 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TEST_BIGINT_SUB_H
-# define TEST_BIGINT_SUB_H
+#include "utils.h"
+#include "bigint.h"
 
-int		test_bigint_sub_bigint_launcher(void);
-int		test_bigint_sub_self_launcher(void);
+int		test_bigint_sub_self_int_null(void)
+{
+	t_bigint	a;
+	int			n;
+	t_bigint	expected;
+	int			diff;	
 
-#endif
+	a = (t_bigint){2, {256, 3245460, 0, 0, 0}};
+	n = 0;
+	expected = (t_bigint){2, {256, 3245460, 0, 0, 0}};
+
+	bigint_sub_int(&a, &a, n);
+	diff = bigint_cmp(&a, &expected);
+	if (diff)
+		print_diff_bigint(0, 0, &a, &expected, diff);	
+	return (diff);
+}
