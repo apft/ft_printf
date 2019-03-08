7f22ea1814d8e7c0de81ed78d32ac0168b7f10d2
Alexandre PION <apion@student.42.fr>
2019-03-08 17:25:44 +0100

    dev[wip]: beginning of ryan juckett algo

--- test/test_printf/test_float/test_float_conv.c
+++ test/test_printf/test_float/test_float_conv.c
@@ -17,10 +17,12 @@ int		test_float_conv(void)
 
 	bzero(str_printf, BUFF_SIZE);
 	//format = "|%f|%10f|%10.f|%10.0f|%.1f|%10.2f|%10.5f|";
-	format = "|%f|";
+	format = "|%f|%f|%f|";
 	n = 4.423547891112;
-	ret_exp = sprintf(str_printf, format, n, n, n, n, n, n, n);
-	ret = ft_asprintf(&out, format, n, n, n, n, n, n, n);
+	double p = 0.000000343549217;
+	double q = 34354.9217;
+	ret_exp = sprintf(str_printf, format, n, p, q, n, n, n, n);
+	ret = ft_asprintf(&out, format, n, p, q, n, n, n, n);
 
 	diff = strcmp(out, str_printf);
 	error = ret != ret_exp || diff;
