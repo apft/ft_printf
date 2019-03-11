0a1047942b3bf7072339ad59ffd174c24b3d8be6
Alexandre PION <apion@student.42.fr>
2019-03-11 19:20:53 +0100

    test: add test value for double

--- test/test_printf/test_float/test_float_conv.c
+++ test/test_printf/test_float/test_float_conv.c
@@ -18,15 +18,16 @@ int		test_float_conv(void)
 
 	bzero(str_printf, BUFF_SIZE);
 	//format = "|%f|%10f|%10.f|%10.0f|%.1f|%10.2f|%10.5f|";
-	format = "|%.50f|%.75f|%.40f|%f|";
+	format = "|%.50f|%.75f|%.40f|%f|%.50f|";
 //	format = "|%f|";
 	n = 4.423547891112;
 	double p = 0.000000343549217;
 	double q = 34354.9217;
 	double max = DBL_MAX;
+	double r = 0x1.0254p+0;
 	printf("dbl_max: %f\n", max);
-	ret_exp = sprintf(str_printf, format, n, p, q, max, n, n, n);
-	ret = ft_asprintf(&out, format, n, p, q, max, n, n, n);
+	ret_exp = sprintf(str_printf, format, n, p, q, max, r, n, n);
+	ret = ft_asprintf(&out, format, n, p, q, max, r, n, n);
 
 	diff = strcmp(out, str_printf);
 	error = ret != ret_exp || diff;
