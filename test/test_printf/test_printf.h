0bbf2eecf0455ae3d56ae786ef22817f58b79af9
Alexandre PION <apion@student.42.fr>
2019-03-11 19:18:25 +0100

    test: fix compilation error

--- test/test_printf/test_printf.h
+++ test/test_printf/test_printf.h
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 18:23:10 by apion             #+#    #+#             */
-/*   Updated: 2019/03/07 12:01:54 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/11 19:02:27 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int		test_extract_float_conv_hex_launcher(void);
 int		test_extract_str_char_conv_launcher(void);
 int		test_extract_ptr_percent_conv_launcher(void);
 int		test_bigint_launcher(void);
+int		test_extract_float_launcher(void);
 
 #endif
