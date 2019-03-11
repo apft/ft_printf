7f1bde88872c461521f31469e5362deb3ef81f78
Alexandre PION <apion@student.42.fr>
2019-03-11 18:25:18 +0100

    dev: improve bigint sub algo

--- srcs/bigint/sub.c
+++ srcs/bigint/sub.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/16 18:19:35 by apion             #+#    #+#             */
-/*   Updated: 2019/03/11 15:39:49 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/11 18:21:21 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,25 @@ static void	sub_and_shrink_length(t_bigint *result, t_bigint *a, t_bigint *b)
 {
 	unsigned int	i;
 	int				n_null_block;
-	unsigned int	alpha;
-	unsigned int	beta;
-	unsigned int	carry;
+	unsigned long	alpha;
+	unsigned long	beta;
+	unsigned long	carry;
 
 	n_null_block = 0;
 	carry = 0;
 	i = 0;
-	while (i < result->length)
+	while (i < a->length)
 	{
-		alpha = a->blocks[i];
-		beta = b->blocks[i];
-		a->blocks[i] += carry;
-		if (alpha >= beta)
-			result->blocks[i] = alpha - beta;
+		alpha = (unsigned long)a->blocks[i];
+		beta = (unsigned long)b->blocks[i];
+		if (alpha >= beta + carry)
+		{
+			result->blocks[i] = (unsigned int)((alpha - beta - carry) & BIGINT_MASK_BLOCK);
+			carry = 0;
+		}
 		else
 		{
-			a->blocks[i + 1] -= 1;
-			result->blocks[i] = (unsigned int)((1UL << 32) + alpha - beta);
+			result->blocks[i] = (unsigned int)((1UL << BIGINT_SIZE_BLOCK) + alpha - beta - carry);
 			carry = 1;
 		}
 		n_null_block = (result->blocks[i] == 0 && i) ? n_null_block + 1 : 0;
@@ -52,12 +53,16 @@ void		bigint_sub_int(t_bigint *result, t_bigint *a, unsigned int n)
 
 void		bigint_sub(t_bigint *result, t_bigint *a, t_bigint *b)
 {
+	t_bigint		temp;
+
 	if (bigint_is_underflow(a) || bigint_is_underflow(b)
 			|| bigint_cmp(a, b) < 0)
 		result->blocks[BIGINT_N_BLOCKS] = BIGINT_UNDERFLOW;
 	else
 	{
-		result->length = a->length;
-		sub_and_shrink_length(result, a, b);
+		bigint_init_null(&temp);
+		temp.length = a->length;
+		sub_and_shrink_length(&temp, a, b);
+		bigint_copy(result, &temp);
 	}
 }
