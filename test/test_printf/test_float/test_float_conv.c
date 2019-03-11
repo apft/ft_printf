13d6bffa2de2c89846a2ebb97d996677c82b0ff5
Alexandre PION <apion@student.42.fr>
2019-03-11 18:46:22 +0100

    test: bigint double digit extraction

--- test/test_printf/test_float/test_float_conv.c
+++ test/test_printf/test_float/test_float_conv.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <float.h>
 #include "ft_printf.h"
 #include "utils.h"
 
@@ -17,12 +18,15 @@ int		test_float_conv(void)
 
 	bzero(str_printf, BUFF_SIZE);
 	//format = "|%f|%10f|%10.f|%10.0f|%.1f|%10.2f|%10.5f|";
-	format = "|%f|%f|%f|";
+	format = "|%.50f|%.75f|%.40f|%f|";
+//	format = "|%f|";
 	n = 4.423547891112;
 	double p = 0.000000343549217;
 	double q = 34354.9217;
-	ret_exp = sprintf(str_printf, format, n, p, q, n, n, n, n);
-	ret = ft_asprintf(&out, format, n, p, q, n, n, n, n);
+	double max = DBL_MAX;
+	printf("dbl_max: %f\n", max);
+	ret_exp = sprintf(str_printf, format, n, p, q, max, n, n, n);
+	ret = ft_asprintf(&out, format, n, p, q, max, n, n, n);
 
 	diff = strcmp(out, str_printf);
 	error = ret != ret_exp || diff;
