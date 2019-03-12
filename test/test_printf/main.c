09948349a591c07eaef19702f8e4e2fca29b65ae
Alexandre PION <apion@student.42.fr>
2019-03-12 11:32:11 +0100

    test: improve float tests + reapply float_hexa tests

--- test/test_printf/main.c
+++ test/test_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 14:51:35 by apion             #+#    #+#             */
-/*   Updated: 2019/03/11 19:02:05 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/12 11:12:09 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		main(void)
 
 	n = 0;
 //	n += test_extract_int_conv_launcher();
-//	n += test_extract_float_conv_hex_launcher();
+	n += test_extract_float_conv_hex_launcher();
 //	n += test_extract_str_char_conv_launcher();
 //	n += test_extract_ptr_percent_conv_launcher();
 //	n += test_bigint_launcher();
--- test/test_printf/test_float/test_float_conv.c
+++ test/test_printf/test_float/test_float_conv.c
@@ -17,17 +17,21 @@ int		test_float_conv(void)
 	double		n;
 
 	bzero(str_printf, BUFF_SIZE);
-	//format = "|%f|%10f|%10.f|%10.0f|%.1f|%10.2f|%10.5f|";
-	format = "|%.50f|%.75f|%.40f|%f|%.50f|";
+	format = "|%.50f|%.75f|%.40f|%f|%.50f|\n";
 //	format = "|%f|";
 	n = 4.423547891112;
 	double p = 0.000000343549217;
 	double q = 34354.9217;
 	double max = DBL_MAX;
 	double r = 0x1.0254p+0;
-	printf("dbl_max: %f\n", max);
+	printf("%s", format);
 	ret_exp = sprintf(str_printf, format, n, p, q, max, r, n, n);
 	ret = ft_asprintf(&out, format, n, p, q, max, r, n, n);
+//	format = "|%f|%10f|%10.f|%10.0f|%.1f|%10.2f|%10.5f|\n";
+//	printf("%s", format);
+//	printf(format, max, max, max, max, max, max, max, max);
+//	printf(format, p, p, p, p, p, p, p, p);
+//	printf(format, q, q, q, q, q, q, q, q);
 
 	diff = strcmp(out, str_printf);
 	error = ret != ret_exp || diff;
