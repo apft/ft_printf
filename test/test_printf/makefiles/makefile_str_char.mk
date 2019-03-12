b826987126da174b6abad168562979ee00cfb065
Alexandre PION <apion@student.42.fr>
2019-03-12 17:21:35 +0100

    test: fix str_char makefile

--- test/test_printf/makefiles/makefile_str_char.mk
+++ test/test_printf/makefiles/makefile_str_char.mk
@@ -6,21 +6,21 @@
 #    By: apion <apion@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/15 14:39:55 by apion             #+#    #+#              #
-#    Updated: 2019/02/15 14:39:58 by apion            ###   ########.fr        #
+#    Updated: 2019/03/12 17:19:36 by apion            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-C_FILES_STR := test_str_char/00_launcher.c \
-				test_str_char/01_test_char_easy.c \
-				test_str_char/02_test_char_medium.c \
-				test_str_char/03_test_char_hard.c \
-				test_str_char/04_test_char_null.c \
-				test_str_char/05_test_string_medium.c \
-				test_str_char/06_test_string_medium_other.c \
-				test_str_char/07_test_string_hard.c \
-				test_str_char/08_test_string_null_mix.c \
-				test_str_char/09_test_str_char_null.c \
-				test_str_char/10_test_mix_all_null.c \
-				test_str_char/11_test_mix_null_medium.c \
-				test_str_char/12_test_undef_type.c \
-				test_str_char/13_test_undef_type_other.c
+C_FILES_STR_CHAR := test_str_char/00_launcher.c \
+	test_str_char/01_test_char_easy.c \
+	test_str_char/02_test_char_medium.c \
+	test_str_char/03_test_char_hard.c \
+	test_str_char/04_test_char_null.c \
+	test_str_char/05_test_string_medium.c \
+	test_str_char/06_test_string_medium_other.c \
+	test_str_char/07_test_string_hard.c \
+	test_str_char/08_test_string_null_mix.c \
+	test_str_char/09_test_str_char_null.c \
+	test_str_char/10_test_mix_all_null.c \
+	test_str_char/11_test_mix_null_medium.c \
+	test_str_char/12_test_undef_type.c \
+	test_str_char/13_test_undef_type_other.c
