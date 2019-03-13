d0f1cf70e7b640549ddb02d21fa2a23dd2ad21fb
Alexandre PION <apion@student.42.fr>
2019-03-13 16:50:57 +0100

    test: float all tests are GREEN

--- test/test_printf/main.c
+++ test/test_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 14:51:35 by apion             #+#    #+#             */
-/*   Updated: 2019/03/12 20:04:57 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/13 16:44:04 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ int		main(void)
 	int		n;
 
 	n = 0;
-//	n += test_extract_int_conv_launcher();
-//	n += test_extract_str_char_conv_launcher();
-//	n += test_extract_ptr_percent_conv_launcher();
+	n += test_extract_int_conv_launcher();
+	n += test_extract_str_char_conv_launcher();
+	n += test_extract_ptr_percent_conv_launcher();
 	n += test_extract_float_conv_hex_launcher();
 //	n += test_bigint_launcher();
-	n += test_extract_float_launcher();
+	n += test_extract_float_conv_launcher();
 	printf("TOTAL ERROR= %d\n", n);
 	if (n)
 		return (1);
