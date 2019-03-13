ac5b6083e97e48bde3ef4e94249093f913fac497
Alexandre PION <apion@student.42.fr>
2019-03-13 11:35:40 +0100

    dev: fix infinite loop with null float

--- srcs/utils_float.c
+++ srcs/utils_float.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 15:02:09 by apion             #+#    #+#             */
-/*   Updated: 2019/03/12 13:35:08 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/13 11:30:59 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ int		pf_compute_float_pow_ten(double n)
 {
 	int		pow_ten;
 
+	pow_ten = 0;
+	if (!n)
+		return (pow_ten);
 	if (n < 0)
 		n *= -1;
-	pow_ten = 0;
 	if (n > 10.0)
 	{
 		while (n > 10.0)
