ecd9d9ff4dbeb37b215408aaf83b8ed044e5a490
Alexandre PION <apion@student.42.fr>
2019-03-14 18:17:49 +0100

    test: comment unused tests

--- test/test_printf/main.c
+++ test/test_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 14:51:35 by apion             #+#    #+#             */
-/*   Updated: 2019/03/13 16:44:04 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/14 17:36:28 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ int		main(void)
 	int		n;
 
 	n = 0;
-	n += test_extract_int_conv_launcher();
-	n += test_extract_str_char_conv_launcher();
-	n += test_extract_ptr_percent_conv_launcher();
-	n += test_extract_float_conv_hex_launcher();
+//	n += test_extract_int_conv_launcher();
+//	n += test_extract_str_char_conv_launcher();
+//	n += test_extract_ptr_percent_conv_launcher();
+//	n += test_extract_float_conv_hex_launcher();
 //	n += test_bigint_launcher();
 	n += test_extract_float_conv_launcher();
 	printf("TOTAL ERROR= %d\n", n);
