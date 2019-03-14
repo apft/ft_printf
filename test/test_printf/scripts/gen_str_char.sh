c362ae5c5947edef9bd894c99dcd58062f001cec
Alexandre PION <apion@student.42.fr>
2019-03-14 15:39:04 +0100

    test:  auto generate makefile for str + ptr scripts

--- test/test_printf/scripts/gen_str_char.sh
+++ test/test_printf/scripts/gen_str_char.sh
@@ -8,11 +8,23 @@ data="${TEST_TYPE}_format.txt"
 template_folder="template"
 template_launcher=${template_folder}/00_launcher_${TEST_TYPE}.c
 template_header=${template_folder}/test_${TEST_TYPE}.h
+template_makefile=${template_folder}/makefile_${TEST_TYPE}.mk
 
-folder="../test_${TEST_TYPE}"
-[ ! -d "$folder" ] && mkdir $folder
+folder_name="test_${TEST_TYPE}"
+folder="../${folder_name}"
 launcher="$folder/00_launcher.c"
 header="$folder/test_${TEST_TYPE}.h"
+
+makefile_folder="../makefiles"
+makefile="${makefile_folder}/makefile_${TEST_TYPE}.mk"
+
+if [ "$1" == "new" ]; then
+	[ "$debug" -eq 1 ] && echo "Redo all files" && echo "clear all old files"
+	[ -f $makefile ] && rm $makefile
+	[ -d $folder ] && rm -rf $folder
+fi
+
+[ ! -d "$folder" ] && mkdir $folder
 if [ ! -f "$launcher" ]
 then
 	[ "$debug" -eq 1 ] && echo "get template file: ${template_launcher}"
@@ -22,6 +34,11 @@ if [ ! -f "$header" ]
 then
 	[ "$debug" -eq 1 ] && echo "get template file: ${template_header}"
 	cp ${template_header} $header
+fi
+if [ ! -f "$makefile" ]
+then
+	[ "$debug" -eq 1 ] && echo "get template file: ${template_makefile}"
+	cp ${template_makefile} $makefile
 fi
 
 includes="<stdio.h> <string.h> <stdlib.h> \"ft_printf.h\" \"utils.h\""
@@ -80,7 +97,13 @@ do
 			tail -n 2 $header
 		} > $header.tmp
 		mv $header.tmp $header
+		delim="\x5c"
+		{
+			echo "\t${folder_name}/$file ${delim}"
+		} >> $makefile.tmp
 	else
 		echo "nothing to be done for file: $file"
 	fi
 done < $data
+sed '$ s/..$//' ${makefile}.tmp >> ${makefile}
+rm ${makefile}.tmp
--- test/test_printf/scripts/template/makefile_ptr_percent.mk
+++ test/test_printf/scripts/template/makefile_ptr_percent.mk
@@ -6,12 +6,8 @@
 #    By: apion <apion@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/15 14:40:06 by apion             #+#    #+#              #
-#    Updated: 2019/02/15 14:40:08 by apion            ###   ########.fr        #
+#    Updated: 2019/03/14 15:34:02 by apion            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 C_FILES_PTR_PERCENT := test_ptr_percent/00_launcher.c \
-						test_ptr_percent/01_test_ptr.c \
-						test_ptr_percent/02_test_ptr_null.c \
-						test_ptr_percent/03_test_percent.c \
-						test_ptr_percent/04_test_percent_hard.c
