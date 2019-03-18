4c870540933248f7885547988b38c6b1e287342c
Alexandre PION <apion@student.42.fr>
2019-03-18 13:22:20 +0100

    test: all tests are green

--- test/test_printf/main.c
+++ test/test_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 14:51:35 by apion             #+#    #+#             */
-/*   Updated: 2019/03/15 22:24:08 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/18 12:02:40 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int		main(void)
 	int		n;
 
 	n = 0;
-//	n += test_extract_int_conv_launcher();
-//	n += test_extract_str_char_conv_launcher();
-//	n += test_extract_ptr_percent_conv_launcher();
-	n += test_bigint_launcher();
-//	n += test_float_launcher();
+	n += test_extract_int_conv_launcher();
+	n += test_extract_str_char_conv_launcher();
+	n += test_extract_ptr_percent_conv_launcher();
+//	n += test_bigint_launcher();
+	n += test_float_launcher();
 	printf("TOTAL ERROR= %d\n", n);
 	if (n)
 		return (1);
