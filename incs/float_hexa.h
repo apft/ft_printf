9d824b44efed7edcee8900bc896c2aa23217c3b2
Alexandre PION <apion@student.42.fr>
2019-03-20 20:36:04 +0100

    dev: refactor float hexa + norm

--- incs/float_hexa.h
+++ incs/float_hexa.h
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 19:47:49 by apion             #+#    #+#             */
-/*   Updated: 2019/03/20 20:14:10 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/20 20:32:23 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FLOAT_HEXA_H
 
 unsigned int	float_hexa_extract_byte(unsigned long n, int byte_index);
-void			float_hexa_round(unsigned long n, char *base, char *str,
+int				float_hexa_round(unsigned long n, char *base, char *str,
 								int precision);
 
 #endif
