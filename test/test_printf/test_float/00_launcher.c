84494ff79c25af05f1ba5d2bb872cbd185ab0a88
Alexandre PION <apion@student.42.fr>
2019-03-20 12:59:02 +0100

    test: all up and good

--- test/test_printf/test_float/00_launcher.c
+++ test/test_printf/test_float/00_launcher.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/15 19:49:21 by apion             #+#    #+#             */
-/*   Updated: 2019/03/18 18:04:01 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/20 12:57:13 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int		test_float_launcher()
 	int		n;
 
 	n = 0;
-//	n += test_extract_float_conv_launcher();
-//	n += test_extract_float_conv_hexa_launcher();
-//	n += test_extract_float_conv_subnormal_launcher();
+	n += test_extract_float_conv_launcher();
+	n += test_extract_float_conv_hexa_launcher();
+	n += test_extract_float_conv_subnormal_launcher();
 	n += test_extract_float_conv_limits_launcher();
 	n += test_extract_float_conv_long_dbl_launcher();
 	return (n);
