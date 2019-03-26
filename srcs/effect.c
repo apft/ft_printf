438863a20c8c9adda1d8210d596be0c4ed23afc7
Alexandre PION <apion@student.42.fr>
2019-03-26 11:30:33 +0100

    effect: fix loop exit + new graphic rendition support

--- srcs/effect.c
+++ srcs/effect.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/25 16:48:58 by apion             #+#    #+#             */
-/*   Updated: 2019/03/25 18:41:56 by apion            ###   ########.fr       */
+/*   Updated: 2019/03/26 11:26:40 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,76 @@ t_effect		g_effect[] =
 	{"eoc", RESET},
 	{"nc", RESET},
 	{"", RESET},
+
+	{"b", BOLD},
 	{"bold", BOLD},
 	{"dim", DIM},
-	{"itatic", ITALIC},
+	{"italic", ITALIC},
+	{"u", UNDERLINE},
 	{"underline", UNDERLINE},
 	{"reverse", REVERSE_VIDEO},
 	{"crossed", CROSSED},
 	{"strike", CROSSED},
+
+	{"\\b", BOLD_OFF},
+	{"reset_bold", BOLD_OFF},
+	{"clear_bold", BOLD_OFF},
+	{"end_bold", BOLD_OFF},
+	{"reset_dim", DIM_OFF},
+	{"clear_dim", DIM_OFF},
+	{"end_dim", DIM_OFF},
+	{"reset_italic", ITALIC_OFF},
+	{"clear_italic", ITALIC_OFF},
+	{"end_italic", ITALIC_OFF},
+	{"\\u", UNDERLINE_OFF},
+	{"reset_underline", UNDERLINE_OFF},
+	{"clear_underline", UNDERLINE_OFF},
+	{"end_underline", UNDERLINE_OFF},
+	{"reset_reverse", REVERSE_OFF},
+	{"clear_reverse", REVERSE_OFF},
+	{"end_reverse", REVERSE_OFF},
+	{"reset_crossed", CROSSED_OFF},
+	{"clear_crossed", CROSSED_OFF},
+	{"end_crossed", CROSSED_OFF},
+
+	{"\\fg", DEFAULT_FG},
+	{"no_fg", DEFAULT_FG},
+	{"\\bg", DEFAULT_BG},
+	{"no_bg", DEFAULT_BG},
+
 	{"black", BLACK},
+	{"black_fg", BLACK},
 	{"red", RED},
+	{"red_fg", RED},
 	{"green", GREEN},
+	{"green_fg", GREEN},
 	{"yellow", YELLOW},
+	{"yellow_fg", YELLOW},
 	{"blue", BLUE},
+	{"blue_fg", BLUE},
 	{"magenta", MAGENTA},
+	{"magenta_fg", MAGENTA},
 	{"cyan", CYAN},
+	{"cyan_fg", CYAN},
 	{"white", WHITE},
+	{"white_fg", WHITE},
+	{"light_black", LIGHT_BLACK},
+	{"light_black_fg", LIGHT_BLACK},
+	{"light_red", LIGHT_RED},
+	{"light_red_fg", LIGHT_RED},
+	{"light_green", LIGHT_GREEN},
+	{"light_green_fg", LIGHT_GREEN},
+	{"light_yellow", LIGHT_YELLOW},
+	{"light_yellow_fg", LIGHT_YELLOW},
+	{"light_blue", LIGHT_BLUE},
+	{"light_blue_fg", LIGHT_BLUE},
+	{"light_magenta", LIGHT_MAGENTA},
+	{"light_magenta_fg", LIGHT_MAGENTA},
+	{"light_cyan", LIGHT_CYAN},
+	{"light_cyan_fg", LIGHT_CYAN},
+	{"light_white", LIGHT_WHITE},
+	{"light_white_fg", LIGHT_WHITE},
+
 	{"black_bg", BLACK_BG},
 	{"red_bg", RED_BG},
 	{"green_bg", GREEN_BG},
@@ -44,14 +99,14 @@ t_effect		g_effect[] =
 	{"magenta_bg", MAGENTA_BG},
 	{"cyan_bg", CYAN_BG},
 	{"white_bg", WHITE_BG},
-	{"light_black", LIGHT_BLACK},
-	{"light_red", LIGHT_RED},
-	{"light_green", LIGHT_GREEN},
-	{"light_yellow", LIGHT_YELLOW},
-	{"light_blue", LIGHT_BLUE},
-	{"light_magenta", LIGHT_MAGENTA},
-	{"light_cyan", LIGHT_CYAN},
-	{"light_white", LIGHT_WHITE}
+	{"light_black_bg", LIGHT_BLACK_BG},
+	{"light_red_bg", LIGHT_RED_BG},
+	{"light_green_bg", LIGHT_GREEN_BG},
+	{"light_yellow_bg", LIGHT_YELLOW_BG},
+	{"light_blue_bg", LIGHT_BLUE_BG},
+	{"light_magenta_bg", LIGHT_MAGENTA_BG},
+	{"light_cyan_bg", LIGHT_CYAN_BG},
+	{"light_white_bg", LIGHT_WHITE_BG}
 };
 
 static int	effect_match(const char **effect, char *expected)
@@ -61,7 +116,8 @@ static int	effect_match(const char **effect, char *expected)
 	i = 1;
 	if (!effect || !*effect || !expected)
 		return (0);
-	while (*(*effect + i) != '}' && *expected && *(*effect + i) == *expected++)
+	while (**effect && *(*effect + i) != '}' && *expected
+			&& *(*effect + i) == *expected++)
 		++i;
 	if (*(*effect + i) == '}' && !*expected)
 	{
